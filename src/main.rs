64ffb0546d95488b9cc99312a994fbd2343b3a7a
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-11 23:48:41 +0300

    Add integration test for health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,18 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::configure_app;
+    use actix_web::{http::StatusCode, test, App};
+
+    #[actix_web::test]
+    async fn health_check_test() {
+        let app = test::init_service(App::new().configure(configure_app)).await;
+        let req = test::TestRequest::get().uri("/health_check").to_request();
+
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
