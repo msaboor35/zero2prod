b7c348f1a0a3fc9f2791b62d475605b38f403e76
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-11 21:37:51 +0300

    Add health check service

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "Hello, World!"
-}
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", &name)
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
+    HttpServer::new(|| App::new().service(health_check))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
