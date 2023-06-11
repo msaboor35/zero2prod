1bb31b5806ce9bc26dc7fabc5ee21e8039d16779
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-11 23:47:45 +0300

    Separate app configuration in a separate method

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+fn configure_app(cfg: &mut web::ServiceConfig) {
+    cfg.service(health_check);
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
+    HttpServer::new(|| App::new().configure(configure_app))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
