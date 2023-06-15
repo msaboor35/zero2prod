051d41238c897f658c57e8402f7165249ab22e5e
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-15 19:53:17 +0300

    Create subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
 use actix_web::dev::Server;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct SubscriptionForm {
+    name: String,
+    email: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<SubscriptionForm>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn configure_app(cfg: &mut web::ServiceConfig) {
-    cfg.service(health_check);
+    cfg.service(health_check).service(subscribe);
 }
 
 pub fn run() -> Result<Server, std::io::Error> {
