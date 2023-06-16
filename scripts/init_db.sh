bb37dff78febf0107a7179c12cead45b6d9a8bba
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-16 12:07:25 +0300

    Add initialization script for database

---
+++ scripts/init_db.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    exit 1
+fi
+
+DB_HOST=${POSTGRES_HOST:=localhost}
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD=${POSTGRES_PASSWORD:=postgres}
+DB_NAME=${POSTGRES_DB:=newsletter}
+DB_PORT=${POSTGRES_PORT:=5432}
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d postgres -c '\q'; do
+    >&2 echo "Postgres is not ready - sleeping 1 second"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}"
+sqlx database create
