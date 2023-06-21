298a0b7f3946d100e61d089185838898ae34d753
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-21 13:05:24 +0300

    Add database connection in web state

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,21 @@ pub mod startup;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use routes::{health_check, subscribe};
+use sqlx::PgConnection;
 
 pub fn configure_app(cfg: &mut web::ServiceConfig) {
     cfg.service(health_check).service(subscribe);
 }
 
-pub fn run(port: u16) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().configure(configure_app))
-        .bind(("127.0.0.1", port))?
-        .run();
+pub fn run(port: u16, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
+        App::new()
+            .configure(configure_app)
+            .app_data(connection.clone())
+    })
+    .bind(("127.0.0.1", port))?
+    .run();
 
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use sqlx::{Connection, PgConnection};
 use zero2prod::{configuration::get_configuration, run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = get_configuration().expect("Failed to read configuration");
     println!("{:?}", config);
-    run(config.port)?.await
+    let connection = PgConnection::connect(&config.db.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    run(config.port, connection)?.await
 }
