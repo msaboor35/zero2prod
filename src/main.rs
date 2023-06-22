d7da150c41d7d73c56ce1095c844cfef25faa990
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-22 18:21:10 +0300

    Refactor telemetry initialization into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use zero2prod::{configuration::get_configuration, run, startup::init_db};
+use zero2prod::{
+    configuration::get_configuration,
+    run,
+    startup::init_db,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
     init_db().await;
