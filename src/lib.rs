865ff8c3e42678453aeb351137a25d273986fc10
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-06-22 14:23:05 +0300

    Setup actix logging middleware

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,15 @@ pub mod routes;
 pub mod startup;
 
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 use startup::configure_app;
 
 pub fn run(port: u16) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(move || App::new().configure(configure_app))
-        .bind(("127.0.0.1", port))?
-        .run();
+    let server =
+        HttpServer::new(move || App::new().wrap(Logger::default()).configure(configure_app))
+            .bind(("127.0.0.1", port))?
+            .run();
 
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use zero2prod::{configuration::get_configuration, run, startup::init_db};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let config = get_configuration().expect("Failed to read configuration");
     init_db().await;
     println!("{:?}", config);
