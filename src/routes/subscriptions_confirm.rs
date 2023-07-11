6cd1e9eb6e4ee29d7ba452affce74a4151d501bf
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-07-11 16:19:29 +0300

    Return 400 when parameters are missing to route to confirm subscription

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,8 +1,13 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
+
+#[derive(serde::Deserialize)]
+struct Parameters {
+    token: String,
+}
 
 #[allow(clippy::async_yields_async)]
-#[tracing::instrument(name = "Confirm a pending subscriber")]
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(_parameters))]
 #[get("/subscriptions/confirm")]
-async fn confirm() -> impl Responder {
+async fn confirm(_parameters: web::Query<Parameters>) -> impl Responder {
     HttpResponse::Ok()
 }
