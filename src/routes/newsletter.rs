9bb248e26fbe1ebd2b7f7b3eea426cebb1e8e5f8
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-07-19 15:08:00 +0300

    Obtain list of confirmed subscribers from the database

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,4 +1,7 @@
-use actix_web::{post, HttpResponse, Responder};
+use actix_web::{post, web, HttpResponse, Responder, ResponseError};
+use sqlx::PgPool;
+
+use super::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 struct NewsletterBody {
@@ -12,9 +15,55 @@ struct NewsletterContent {
     text: String,
 }
 
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> actix_http::StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => actix_http::StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 #[allow(clippy::async_yields_async, clippy::let_with_type_underscore)]
-#[tracing::instrument(name = "Publish newsletter request", skip(_body))]
+#[tracing::instrument(name = "Publish newsletter request", skip(_body, pool))]
 #[post("/newsletter")]
-async fn publish_newsletter(_body: actix_web::web::Json<NewsletterBody>) -> impl Responder {
-    HttpResponse::Ok()
+async fn publish_newsletter(
+    _body: web::Json<NewsletterBody>,
+    pool: web::Data<PgPool>,
+) -> Result<impl Responder, PublishError> {
+    let _subscribers = get_confirmed_subscribers(&pool).await?;
+    Ok(HttpResponse::Ok())
+}
+
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[tracing::instrument(name = "Create subscription request", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(rows)
 }
