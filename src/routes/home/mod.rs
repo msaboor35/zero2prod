1bf542c52fdda1a757b83a2de583dc7789c8bf53
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-07-21 14:51:20 +0300

    Fix instrumentation log

--- src/routes/home/mod.rs
+++ src/routes/home/mod.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, http::header::ContentType, HttpResponse, Responder};
 
 #[allow(clippy::async_yields_async, clippy::let_with_type_underscore)]
-#[tracing::instrument(name = "Health check request")]
+#[tracing::instrument(name = "Home page")]
 #[get("/")]
 pub async fn home() -> impl Responder {
     HttpResponse::Ok()
