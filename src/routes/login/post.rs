3ddb06ffcde3c7a51391c1140bc02c625d745ebf
Muhammad Saboor <muhammad.saboor@trapmine.com>
2023-07-21 16:11:40 +0300

    Redirect after form submission

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,11 @@
+use actix_web::http::header;
 use actix_web::{post, HttpResponse, Responder};
 
 #[allow(clippy::async_yields_async, clippy::let_with_type_underscore)]
 #[tracing::instrument(name = "Process login request")]
 #[post("/login")]
 pub async fn login() -> impl Responder {
-    HttpResponse::Ok()
+    let mut resp = HttpResponse::SeeOther();
+    resp.insert_header((header::LOCATION, "/"));
+    resp
 }
